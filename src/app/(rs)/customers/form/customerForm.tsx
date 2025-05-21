04c5664d594d17526183c1063f2ca1eb2a861cc8
Noman Aijaz <nomikk08@gmail.com>
2025-05-21 12:45:37 +0500

    Added customer form

---
+++ src/app/(rs)/customers/form/customerForm.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form"
+import { Button } from "@/components/ui/button";
+import {
+    type selectCustomerSchemaType,
+    type insertCustomerSchemaType,
+    insertCustomerSchema
+} from "@/zod-schemas/customer";
+
+type Props = {
+    customer?: selectCustomerSchemaType,
+}
+
+export default function CustomerForm({ customer }: Props) {
+
+    const defaultValues: insertCustomerSchemaType = {
+        id: customer?.id ?? 0,
+        firstName: customer?.firstName ?? '',
+        lastName: customer?.lastName ?? '',
+        address1: customer?.address1 ?? '',
+        address2: customer?.address2 ?? '',
+        city: customer?.city ?? '',
+        state: customer?.state ?? '',
+        zip: customer?.zip ?? '',
+        phone: customer?.phone ?? '',
+        email: customer?.email ?? '',
+        notes: customer?.notes ?? '',
+    }
+
+    const form = useForm<insertCustomerSchemaType>({
+        mode: 'onBlur',
+        resolver: zodResolver(insertCustomerSchema),
+        defaultValues,
+    })
+
+    async function submitForm(data: insertCustomerSchemaType) {
+        console.log(data)
+    }
+
+    return (
+        <div className="flex flex-col gap-1 sm:px-8">
+            <div>
+                <h2 className="text-2xl font-bold">
+                    {customer?.id ? "Edit" : "New"} Customer Form
+                </h2>
+            </div>
+            <Form {...form}>
+                <form className="flex flex-col gap-4 sm:flex-row sm:gap-8" onSubmit={form.handleSubmit(submitForm)}>
+                    <p>{JSON.stringify(form.getValues())}</p>
+                </form>
+            </Form>
+        </div>
+    )
+}
